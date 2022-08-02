5eba209062fda398633b6aee7cd7880a3c995a71
Ricky Fort <100113249+AriseSon@users.noreply.github.com>
2022-08-02 15:23:03 -0700

    improved indentation and removed import bootstrap on app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 import CatIndex from './pages/CatIndex'
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,20 @@ import MML from '../assets/MML.jpg'
 class Header extends Component {
     render() {
         return (
-            <header>
-              <NavLink to="/">
-                <img src={MML}></img>
-              </NavLink>
-              <div className="nav-links">
-                <ul>
-                  <NavLink to="/catindex">Meet The Community</NavLink>
-                </ul>
-                <ul>
-                  <NavLink to="/catnew">Join The Community</NavLink>
-                </ul>
+          <header>
+            <NavLink to="/">
+              <img src={MML}></img>
+            </NavLink>
+            <div className="nav-links">
+              <ul>
+                <NavLink to="/catindex">Meet The Community</NavLink>
+              </ul>
+              <ul>
+                <NavLink to="/catnew">Join The Community</NavLink>
+              </ul>
             </div> 
         </header>
-        )
+       ) 
     }
 }
 export default Header
